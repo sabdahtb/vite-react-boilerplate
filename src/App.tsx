32eb4b99aabaaff86c61c83fa921a9cae273c2a8
sabdahtb <sabdahutabarat2804@gmail.com>
2024-11-19 14:47:46 +0800

    chore: unit test + .env

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 export default function App() {
+  const envName = process.env.USER_NAME
+
   return (
     <div className="container px-4 py-12">
       <h1 className="text-3xl font-bold">React + Vite!</h1>
+      <pre className="mt-4 text-sm">{JSON.stringify({ envName }, null, 2)}</pre>
     </div>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+import 'dotenv/config'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tsconfigPaths(), react()],
+  define: {
+    'process.env': process.env,
+  },
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true,
+    strictPort: true,
+    port: 4173,
+  },
 })
